09a8a6d267ef4ae0ea86a792ee5f5d567ee8629c
Tim Kurvers <tim@moonsphere.net>
2018-05-13 01:31:15 +0900

    Add webpack configuration

---
+++ packages/spelunker-web/webpack.config.js
@@ -0,0 +1,59 @@
+const path = require('path');
+const webpack = require('webpack');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/bootstrap.jsx',
+  output: {
+    filename: 'spelunker-[hash].js',
+    path: path.resolve(__dirname, 'public'),
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+    new webpack.EnvironmentPlugin([
+      'API_URI',
+      'DEBUG',
+      'PIPELINE_URI',
+    ]),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.styl$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true,
+            },
+          },
+          'stylus-loader',
+        ],
+      },
+      {
+        test: /\.m?jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        use: [
+          'babel-loader',
+        ],
+      },
+    ],
+  },
+
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    historyApiFallback: true,
+    port: process.env.WEB_PORT,
+  },
+  devtool: 'cheap-source-map',
+};
