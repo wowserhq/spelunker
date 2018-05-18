1ebe8f404a4bf256ba50d6b6d2082d5b17f4e16c
Tim Kurvers <tim@moonsphere.net>
2018-05-18 03:04:50 +0200

    Support fixed column queries

---
+++ packages/spelunker-api/src/lib/db/FixedColumnQuery.mjs
@@ -0,0 +1,59 @@
+import Query from '../core/Query';
+import logger from '../utils/logger';
+
+const log = logger('db:fcq');
+
+class FixedColumnQuery extends Query {
+  constructor({ label, start = 1, end, resolve }) {
+    super(null);
+
+    this.label = label;
+    this.start = start,
+    this.end = end;
+    this.resolve = resolve;
+
+    this.offset = 0;
+    this.limit = undefined;
+
+    this.results = null;
+  }
+
+  slice(offset, limit) {
+    this.offset = offset;
+    this.limit = limit;
+    return this;
+  }
+
+  async count() {
+    log(`counting ${this.label}`);
+    const results = await this;
+    return results.length;
+  }
+
+  then(resolve) {
+    if (!resolve) {
+      return null;
+    }
+
+    if (!this.results) {
+      log(`fetching ${this.label}`);
+
+      this.results = [];
+      for (let i = this.start; i <= this.end; ++i) {
+        const entry = this.resolve(i);
+        if (entry) {
+          this.results.push(entry);
+        }
+      }
+    }
+
+    const end = this.limit ? this.offset + this.limit : undefined;
+    resolve(this.results.slice(this.offset, end));
+  }
+
+  static for(...args) {
+    return new this(...args);
+  }
+}
+
+export default FixedColumnQuery;
