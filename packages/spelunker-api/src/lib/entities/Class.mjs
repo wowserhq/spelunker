eb84d9f16d625d51569ba7b085a614a817feb7e6
Tim Kurvers <tim@moonsphere.net>
2018-06-13 13:00:03 +0200

    Support race and class masks

--- packages/spelunker-api/src/lib/entities/Class.mjs
+++ packages/spelunker-api/src/lib/entities/Class.mjs
@@ -4,6 +4,11 @@ class Class extends DBCEntity {
   static get dbc() {
     return 'ChrClasses';
   }
+
+  static async findByMask(mask) {
+    const classes = await Class.query;
+    return classes.filter(klass => (1 << klass.id - 1) & mask);
+  }
 }
 
 export default Class;
--- packages/spelunker-api/src/lib/entities/Race.mjs
+++ packages/spelunker-api/src/lib/entities/Race.mjs
@@ -1,8 +1,24 @@
 import DBCEntity from '../dbc/Entity';
 
+import Side from './Side';
+
 class Race extends DBCEntity {
   static get dbc() {
     return 'ChrRaces';
+  }
+
+  static async findByMask(mask, { exclusive = false }) {
+    if (exclusive) {
+      const sides = await Side.query;
+      sides.forEach(side => {
+        if ((mask & side.racemask) === side.racemask) {
+          mask &= ~side.racemask;
+        }
+      });
+    }
+
+    const races = await Race.query;
+    return races.filter(race => (1 << race.id - 1) & mask);
   }
 
   get filename() {
