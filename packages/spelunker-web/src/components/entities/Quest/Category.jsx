f4ef5c583f91d3b17d869c7097ed7598d14c9002
Tim Kurvers <tim@moonsphere.net>
2018-06-13 13:29:50 +0200

    Add QuestCategory component

---
+++ packages/spelunker-web/src/components/entities/Quest/Category.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import gql from 'graphql-tag';
+
+import AreaReference from '../Area/Reference';
+
+const QuestCategory = ({ category }) => {
+  if (!category) {
+    return null;
+  }
+  if (category.__typename === 'Area') {
+    return <AreaReference area={category} />;
+  }
+  return (
+    <span>{category.name}</span>
+  );
+};
+
+QuestCategory.fragment = gql`
+  fragment QuestCategory on QuestCategory {
+    ... on QuestSort {
+      id
+      name
+    }
+    ... on Area {
+      ...AreaReference
+    }
+  }
+
+  ${AreaReference.fragment}
+`;
+
+export default QuestCategory;
