5b74741eeeb5d8ff7740971f2f51ec7fa85ccc92
Tim Kurvers <tim@moonsphere.net>
2018-06-16 00:08:09 +0200

    Let Blizzardry handle loading MPQ chain

--- packages/spelunker-api/src/lib/mpq/index.mjs
+++ packages/spelunker-api/src/lib/mpq/index.mjs
@@ -1,35 +1,6 @@
-import path from 'path';
-
-import MPQ from 'blizzardry/lib/mpq';
-import glob from 'globby';
+import MPQChain from 'blizzardry/lib/mpq/chain';
 
 const dataDir = process.env.DATA_DIR;
-
-const mpqs = [
-  'common.MPQ',
-  'common-2.MPQ',
-  'expansion.MPQ',
-  'lichking.MPQ',
-  '*/locale-*.MPQ',
-  '*/speech-*.MPQ',
-  '*/expansion-locale-*.MPQ',
-  '*/lichking-locale-*.MPQ',
-  '*/expansion-speech-*.MPQ',
-  '*/lichking-speech-*.MPQ',
-  '*/patch-*.MPQ',
-  'patch.MPQ',
-  'patch-*.MPQ',
-];
-
-const patterns = mpqs.map(mpq => (
-  path.join(dataDir, mpq)
-));
-
-const archives = glob.sync(patterns);
-
-const mpq = MPQ.open(archives.shift(), MPQ.OPEN.READ_ONLY);
-archives.forEach((archive) => {
-  mpq.patch(archive, '');
-});
+const mpq = MPQChain.build(dataDir);
 
 export default mpq;
