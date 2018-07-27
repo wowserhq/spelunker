ae66c70d25230fa6b479047cdf3a2b1c972d516a
Tim Kurvers <tim@moonsphere.net>
2018-07-27 21:09:51 +0200

    Add bare-bones cache utility

---
+++ packages/spelunker-api/src/lib/utils/cache.mjs
@@ -0,0 +1,28 @@
+import logger from './logger';
+
+const SEPARATOR = '/';
+
+const generateCacheKey = (keys) => (
+  keys.map(key => (
+    key instanceof Function ? key.name : key
+  )).join(SEPARATOR)
+);
+
+const createCache = (log = null) => {
+  const cache = new Map();
+
+  return (keys, generator) => {
+    const key = generateCacheKey(keys);
+    if (cache.has(key)) {
+      return cache.get(key);
+    }
+    log && log('populating', key);
+    const value = generator();
+    cache.set(key, value);
+    return value;
+  };
+};
+
+const cache = createCache(logger('cache'));
+export default cache;
+export { createCache, generateCacheKey };
