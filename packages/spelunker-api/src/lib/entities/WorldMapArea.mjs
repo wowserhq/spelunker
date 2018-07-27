3ab7381a6b080651d1330fea6feda11b3e652db8
Tim Kurvers <tim@moonsphere.net>
2018-07-27 23:54:00 +0200

    Allow filtering WorldMapArea by coords

--- packages/spelunker-api/src/lib/entities/WorldMapArea.mjs
+++ packages/spelunker-api/src/lib/entities/WorldMapArea.mjs
@@ -1,8 +1,29 @@
 import DBCEntity from '../dbc/Entity';
 
+import Area from './Area';
+
 class WorldMapArea extends DBCEntity {
   static get dbc() {
     return 'WorldMapArea';
+  }
+
+  static filterByCoords(mapID, x, y) {
+    return this.query.filter(wma => (
+      wma.mapID === mapID &&
+      wma.areaID !== 0 &&
+      wma.position.left >= y &&
+      wma.position.right <= y &&
+      wma.position.top >= x &&
+      wma.position.bottom <= x
+    ));
+  }
+
+  static findByArea(areaID) {
+    return this.query.find(wma => wma.areaID === areaID);
+  }
+
+  area() {
+    return Area.find(this.data.areaID);
   }
 }
 
