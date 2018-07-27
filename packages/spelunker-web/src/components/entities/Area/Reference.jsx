d1ac7a8880ae5a66db1f2018d726d22cf0164baa
Tim Kurvers <tim@moonsphere.net>
2018-07-28 00:19:36 +0200

    Show sides for areas

--- packages/spelunker-web/src/components/entities/Area/Reference.jsx
+++ packages/spelunker-web/src/components/entities/Area/Reference.jsx
@@ -2,17 +2,32 @@ import React from 'react';
 import gql from 'graphql-tag';
 import { Link } from 'react-router-dom';
 
+import SideReference from '../Side/Reference';
+
 const AreaReference = ({ area }) => (
-  <Link to={`/areas/${area.id}`}>
-    {area.name}
-  </Link>
+  <span>
+    {area.sides.results.map(side => (
+      <SideReference key={side.id} side={side} withoutName />
+    ))}
+    <Link to={`/areas/${area.id}`}>
+      {area.name}
+    </Link>
+  </span>
 );
 
 AreaReference.fragment = gql`
   fragment AreaReference on Area {
     id
     name
+
+    sides {
+      results {
+        ...SideReference
+      }
+    }
   }
+
+  ${SideReference.fragment}
 `;
 
 export default AreaReference;
