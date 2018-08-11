2e0bac19f2f9ed4f27410e4c0566556260f6e7b2
Tim Kurvers <tim@moonsphere.net>
2018-08-11 21:50:29 +0200

    Correct boot message referring to port

--- packages/spelunker-api/src/index.mjs
+++ packages/spelunker-api/src/index.mjs
@@ -2,5 +2,5 @@ import server from './lib/server';
 import { log } from './lib/utils/logger';
 
 server.listen(process.env.API_PORT, () => {
-  log(`listening on localhost:${process.env.API_PORT}`);
+  log(`listening on port ${process.env.API_PORT}`);
 });
