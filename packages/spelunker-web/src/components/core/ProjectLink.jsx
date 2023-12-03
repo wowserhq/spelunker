390d2db89ba0f732d5fa11a0e2ef2796002a08c2
fallenoak <git@fallenoak.me>
2023-12-02 21:46:19 -0600

    fix(web): correct link to spelunker github repo

--- packages/spelunker-web/src/components/core/ProjectLink.jsx
+++ packages/spelunker-web/src/components/core/ProjectLink.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const ProjectLink = (props) => (
-  <Link to="https://github.com/wowserhq/spelunker" target="_blank">
+  // eslint-disable-next-line react/jsx-no-target-blank
+  <a href="https://github.com/wowserhq/spelunker" target="_blank">
     {props.label}
-  </Link>
+  </a>
 );
 
 ProjectLink.defaultProps = {
